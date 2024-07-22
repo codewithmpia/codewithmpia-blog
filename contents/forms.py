3b29e6c1532228b4c5571a36d5e588a0d3217de8
codewithmpia <codewithmpia@gmail.com>
2024-07-22 21:56:46 +0200

    Ajout validateur Email

--- contents/forms.py
+++ contents/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import SearchField, StringField, EmailField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 
 class SearchForm(FlaskForm):
